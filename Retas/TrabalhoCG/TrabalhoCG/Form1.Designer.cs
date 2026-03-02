4100c7c12123b18a0d2e1e25c0993e15a4173627
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2026-03-02 11:48:19 -0300

    aaaaa

---
+++ Retas/TrabalhoCG/TrabalhoCG/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TrabalhoCG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.Retas = new System.Windows.Forms.TabPage();
+            this.Circunferencia = new System.Windows.Forms.TabPage();
+            this.rbEqReta = new System.Windows.Forms.RadioButton();
+            this.rbDDA = new System.Windows.Forms.RadioButton();
+            this.rbPMR = new System.Windows.Forms.RadioButton();
+            this.rbEqCircu = new System.Windows.Forms.RadioButton();
+            this.rbTrigo = new System.Windows.Forms.RadioButton();
+            this.rbPMC = new System.Windows.Forms.RadioButton();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl.SuspendLayout();
+            this.Retas.SuspendLayout();
+            this.Circunferencia.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.Retas);
+            this.tabControl.Controls.Add(this.Circunferencia);
+            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(244, 234);
+            this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl.TabIndex = 0;
+            // 
+            // Retas
+            // 
+            this.Retas.Controls.Add(this.rbPMR);
+            this.Retas.Controls.Add(this.rbDDA);
+            this.Retas.Controls.Add(this.rbEqReta);
+            this.Retas.Location = new System.Drawing.Point(4, 22);
+            this.Retas.Name = "Retas";
+            this.Retas.Padding = new System.Windows.Forms.Padding(3);
+            this.Retas.Size = new System.Drawing.Size(236, 208);
+            this.Retas.TabIndex = 1;
+            this.Retas.Text = "Retas";
+            this.Retas.UseVisualStyleBackColor = true;
+            // 
+            // Circunferencia
+            // 
+            this.Circunferencia.Controls.Add(this.rbPMC);
+            this.Circunferencia.Controls.Add(this.rbTrigo);
+            this.Circunferencia.Controls.Add(this.rbEqCircu);
+            this.Circunferencia.Location = new System.Drawing.Point(4, 22);
+            this.Circunferencia.Name = "Circunferencia";
+            this.Circunferencia.Padding = new System.Windows.Forms.Padding(3);
+            this.Circunferencia.Size = new System.Drawing.Size(236, 208);
+            this.Circunferencia.TabIndex = 2;
+            this.Circunferencia.Text = "Circunferência";
+            this.Circunferencia.UseVisualStyleBackColor = true;
+            // 
+            // rbEqReta
+            // 
+            this.rbEqReta.AutoSize = true;
+            this.rbEqReta.Location = new System.Drawing.Point(69, 59);
+            this.rbEqReta.Name = "rbEqReta";
+            this.rbEqReta.Size = new System.Drawing.Size(109, 17);
+            this.rbEqReta.TabIndex = 0;
+            this.rbEqReta.TabStop = true;
+            this.rbEqReta.Text = "Equação da Reta";
+            this.rbEqReta.UseVisualStyleBackColor = true;
+            // 
+            // rbDDA
+            // 
+            this.rbDDA.AutoSize = true;
+            this.rbDDA.Location = new System.Drawing.Point(69, 82);
+            this.rbDDA.Name = "rbDDA";
+            this.rbDDA.Size = new System.Drawing.Size(48, 17);
+            this.rbDDA.TabIndex = 1;
+            this.rbDDA.TabStop = true;
+            this.rbDDA.Text = "DDA";
+            this.rbDDA.UseVisualStyleBackColor = true;
+            // 
+            // rbPMR
+            // 
+            this.rbPMR.AutoSize = true;
+            this.rbPMR.Location = new System.Drawing.Point(69, 107);
+            this.rbPMR.Name = "rbPMR";
+            this.rbPMR.Size = new System.Drawing.Size(85, 17);
+            this.rbPMR.TabIndex = 2;
+            this.rbPMR.TabStop = true;
+            this.rbPMR.Text = "Ponto Médio";
+            this.rbPMR.UseVisualStyleBackColor = true;
+            // 
+            // rbEqCircu
+            // 
+            this.rbEqCircu.AutoSize = true;
+            this.rbEqCircu.Location = new System.Drawing.Point(48, 70);
+            this.rbEqCircu.Name = "rbEqCircu";
+            this.rbEqCircu.Size = new System.Drawing.Size(154, 17);
+            this.rbEqCircu.TabIndex = 1;
+            this.rbEqCircu.TabStop = true;
+            this.rbEqCircu.Text = "Equação da Circunfêrencia";
+            this.rbEqCircu.UseVisualStyleBackColor = true;
+            // 
+            // rbTrigo
+            // 
+            this.rbTrigo.AutoSize = true;
+            this.rbTrigo.Location = new System.Drawing.Point(48, 93);
+            this.rbTrigo.Name = "rbTrigo";
+            this.rbTrigo.Size = new System.Drawing.Size(95, 17);
+            this.rbTrigo.TabIndex = 2;
+            this.rbTrigo.TabStop = true;
+            this.rbTrigo.Text = "Trigonometrica";
+            this.rbTrigo.UseVisualStyleBackColor = true;
+            // 
+            // rbPMC
+            // 
+            this.rbPMC.AutoSize = true;
+            this.rbPMC.Location = new System.Drawing.Point(48, 116);
+            this.rbPMC.Name = "rbPMC";
+            this.rbPMC.Size = new System.Drawing.Size(85, 17);
+            this.rbPMC.TabIndex = 3;
+            this.rbPMC.TabStop = true;
+            this.rbPMC.Text = "Ponto Médio";
+            this.rbPMC.UseVisualStyleBackColor = true;
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(262, 12);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(1206, 817);
+            this.picBox1.TabIndex = 1;
+            this.picBox1.TabStop = false;
+            this.picBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1480, 841);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl.ResumeLayout(false);
+            this.Retas.ResumeLayout(false);
+            this.Retas.PerformLayout();
+            this.Circunferencia.ResumeLayout(false);
+            this.Circunferencia.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage Retas;
+        private System.Windows.Forms.RadioButton rbPMR;
+        private System.Windows.Forms.RadioButton rbDDA;
+        private System.Windows.Forms.RadioButton rbEqReta;
+        private System.Windows.Forms.TabPage Circunferencia;
+        private System.Windows.Forms.RadioButton rbPMC;
+        private System.Windows.Forms.RadioButton rbTrigo;
+        private System.Windows.Forms.RadioButton rbEqCircu;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Retas/TrabalhoCG/TrabalhoCG/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoCG
+{
+    public partial class Form1 : Form
+    {
+        Image img;
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(1200, 800);
+            picBox1.SizeMode = PictureBoxSizeMode.Normal;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+            }
+
+            picBox1.Image = bmp;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void writePixel(int x, int y, Color cor)
+        {
+            if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+            {
+                bmp.SetPixel(x, y, cor);
+                picBox1.Refresh();
+            }
+        }
+
+        public void desenharReta(int x1, int y1, int x2, int y2, Color cor)
+        {
+            double dy = y2 - y1;
+            double dx = x2 - x1;
+            double m = dy / dx;
+
+            for (int x = x1; x <= x2; x++)
+            {
+                {
+                    double y = y1 + m * (x - x1);
+                    writePixel(x, (int)(Math.Round(y)), cor);
+                }
+
+            }
+        }
+    }
+}
