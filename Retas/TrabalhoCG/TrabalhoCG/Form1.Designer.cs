6c188cd158bb1543011a27511a6c12770a0e350b
MaaduRibeiroo <maadu.ribeiro1404@gmail.com>
2026-03-08 18:24:25 -0300

    retas prontas

--- Retas/TrabalhoCG/TrabalhoCG/Form1.Designer.cs
+++ Retas/TrabalhoCG/TrabalhoCG/Form1.Designer.cs
@@ -30,125 +30,136 @@
         {
             this.tabControl = new System.Windows.Forms.TabControl();
             this.Retas = new System.Windows.Forms.TabPage();
-            this.rbPMR = new System.Windows.Forms.RadioButton();
-            this.rbDDA = new System.Windows.Forms.RadioButton();
-            this.rbEqReta = new System.Windows.Forms.RadioButton();
-            this.Circunferencia = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.rbPMC = new System.Windows.Forms.RadioButton();
+            this.rbPMR = new System.Windows.Forms.RadioButton();
             this.rbTrigo = new System.Windows.Forms.RadioButton();
+            this.rbDDA = new System.Windows.Forms.RadioButton();
             this.rbEqCircu = new System.Windows.Forms.RadioButton();
+            this.rbEqReta = new System.Windows.Forms.RadioButton();
             this.picBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.chekBoxReta = new System.Windows.Forms.CheckBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
             this.tabControl.SuspendLayout();
             this.Retas.SuspendLayout();
-            this.Circunferencia.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl
             // 
             this.tabControl.Controls.Add(this.Retas);
-            this.tabControl.Controls.Add(this.Circunferencia);
             this.tabControl.Location = new System.Drawing.Point(12, 12);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(244, 234);
+            this.tabControl.Size = new System.Drawing.Size(244, 489);
             this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControl.TabIndex = 0;
             // 
             // Retas
             // 
-            this.Retas.Controls.Add(this.chekBoxReta);
+            this.Retas.Controls.Add(this.label2);
+            this.Retas.Controls.Add(this.label1);
+            this.Retas.Controls.Add(this.rbPMC);
             this.Retas.Controls.Add(this.rbPMR);
+            this.Retas.Controls.Add(this.rbTrigo);
             this.Retas.Controls.Add(this.rbDDA);
+            this.Retas.Controls.Add(this.rbEqCircu);
             this.Retas.Controls.Add(this.rbEqReta);
             this.Retas.Location = new System.Drawing.Point(4, 22);
             this.Retas.Name = "Retas";
             this.Retas.Padding = new System.Windows.Forms.Padding(3);
-            this.Retas.Size = new System.Drawing.Size(236, 208);
+            this.Retas.Size = new System.Drawing.Size(236, 463);
             this.Retas.TabIndex = 1;
             this.Retas.Text = "Retas";
             this.Retas.UseVisualStyleBackColor = true;
             // 
-            // rbPMR
+            // label2
             // 
-            this.rbPMR.AutoSize = true;
-            this.rbPMR.Location = new System.Drawing.Point(69, 166);
-            this.rbPMR.Name = "rbPMR";
-            this.rbPMR.Size = new System.Drawing.Size(85, 17);
-            this.rbPMR.TabIndex = 2;
-            this.rbPMR.TabStop = true;
-            this.rbPMR.Text = "Ponto Médio";
-            this.rbPMR.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CIRCUFERÊNCIA";
             // 
-            // rbDDA
+            // label1
             // 
-            this.rbDDA.AutoSize = true;
-            this.rbDDA.Location = new System.Drawing.Point(69, 141);
-            this.rbDDA.Name = "rbDDA";
-            this.rbDDA.Size = new System.Drawing.Size(48, 17);
-            this.rbDDA.TabIndex = 1;
-            this.rbDDA.TabStop = true;
-            this.rbDDA.Text = "DDA";
-            this.rbDDA.UseVisualStyleBackColor = true;
-            // 
-            // rbEqReta
-            // 
-            this.rbEqReta.AutoSize = true;
-            this.rbEqReta.Location = new System.Drawing.Point(69, 118);
-            this.rbEqReta.Name = "rbEqReta";
-            this.rbEqReta.Size = new System.Drawing.Size(109, 17);
-            this.rbEqReta.TabIndex = 0;
-            this.rbEqReta.TabStop = true;
-            this.rbEqReta.Text = "Equação da Reta";
-            this.rbEqReta.UseVisualStyleBackColor = true;
-            // 
-            // Circunferencia
-            // 
-            this.Circunferencia.Controls.Add(this.rbPMC);
-            this.Circunferencia.Controls.Add(this.rbTrigo);
-            this.Circunferencia.Controls.Add(this.rbEqCircu);
-            this.Circunferencia.Location = new System.Drawing.Point(4, 22);
-            this.Circunferencia.Name = "Circunferencia";
-            this.Circunferencia.Padding = new System.Windows.Forms.Padding(3);
-            this.Circunferencia.Size = new System.Drawing.Size(236, 208);
-            this.Circunferencia.TabIndex = 2;
-            this.Circunferencia.Text = "Circunferência";
-            this.Circunferencia.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 33);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "RETAS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // rbPMC
             // 
             this.rbPMC.AutoSize = true;
-            this.rbPMC.Location = new System.Drawing.Point(48, 116);
+            this.rbPMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPMC.Location = new System.Drawing.Point(19, 361);
             this.rbPMC.Name = "rbPMC";
-            this.rbPMC.Size = new System.Drawing.Size(85, 17);
+            this.rbPMC.Size = new System.Drawing.Size(105, 21);
             this.rbPMC.TabIndex = 3;
-            this.rbPMC.TabStop = true;
             this.rbPMC.Text = "Ponto Médio";
             this.rbPMC.UseVisualStyleBackColor = true;
+            // 
+            // rbPMR
+            // 
+            this.rbPMR.AutoSize = true;
+            this.rbPMR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPMR.Location = new System.Drawing.Point(19, 142);
+            this.rbPMR.Name = "rbPMR";
+            this.rbPMR.Size = new System.Drawing.Size(105, 21);
+            this.rbPMR.TabIndex = 2;
+            this.rbPMR.Text = "Ponto Médio";
+            this.rbPMR.UseVisualStyleBackColor = true;
             // 
             // rbTrigo
             // 
             this.rbTrigo.AutoSize = true;
-            this.rbTrigo.Location = new System.Drawing.Point(48, 93);
+            this.rbTrigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbTrigo.Location = new System.Drawing.Point(19, 334);
             this.rbTrigo.Name = "rbTrigo";
-            this.rbTrigo.Size = new System.Drawing.Size(95, 17);
+            this.rbTrigo.Size = new System.Drawing.Size(121, 21);
             this.rbTrigo.TabIndex = 2;
-            this.rbTrigo.TabStop = true;
             this.rbTrigo.Text = "Trigonometrica";
             this.rbTrigo.UseVisualStyleBackColor = true;
+            // 
+            // rbDDA
+            // 
+            this.rbDDA.AutoSize = true;
+            this.rbDDA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDDA.Location = new System.Drawing.Point(19, 119);
+            this.rbDDA.Name = "rbDDA";
+            this.rbDDA.Size = new System.Drawing.Size(55, 21);
+            this.rbDDA.TabIndex = 1;
+            this.rbDDA.Text = "DDA";
+            this.rbDDA.UseVisualStyleBackColor = true;
             // 
             // rbEqCircu
             // 
             this.rbEqCircu.AutoSize = true;
-            this.rbEqCircu.Location = new System.Drawing.Point(48, 70);
+            this.rbEqCircu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbEqCircu.Location = new System.Drawing.Point(19, 307);
             this.rbEqCircu.Name = "rbEqCircu";
-            this.rbEqCircu.Size = new System.Drawing.Size(154, 17);
+            this.rbEqCircu.Size = new System.Drawing.Size(197, 21);
             this.rbEqCircu.TabIndex = 1;
-            this.rbEqCircu.TabStop = true;
             this.rbEqCircu.Text = "Equação da Circunfêrencia";
             this.rbEqCircu.UseVisualStyleBackColor = true;
+            // 
+            // rbEqReta
+            // 
+            this.rbEqReta.AutoSize = true;
+            this.rbEqReta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbEqReta.Location = new System.Drawing.Point(19, 96);
+            this.rbEqReta.Name = "rbEqReta";
+            this.rbEqReta.Size = new System.Drawing.Size(136, 21);
+            this.rbEqReta.TabIndex = 0;
+            this.rbEqReta.Text = "Equação da Reta";
+            this.rbEqReta.UseVisualStyleBackColor = true;
             // 
             // picBox1
             // 
@@ -163,31 +174,31 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // chekBoxReta
+            // btnLimpar
             // 
-            this.chekBoxReta.AutoSize = true;
-            this.chekBoxReta.Location = new System.Drawing.Point(54, 56);
-            this.chekBoxReta.Name = "chekBoxReta";
-            this.chekBoxReta.Size = new System.Drawing.Size(98, 17);
-            this.chekBoxReta.TabIndex = 2;
-            this.chekBoxReta.Text = "Desenhar Reta";
-            this.chekBoxReta.UseVisualStyleBackColor = true;
-            this.chekBoxReta.CheckedChanged += new System.EventHandler(this.chekBoxReta_CheckedChanged);
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(77, 536);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(110, 49);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar tela";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1480, 841);
+            this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.picBox1);
             this.Controls.Add(this.tabControl);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl.ResumeLayout(false);
             this.Retas.ResumeLayout(false);
             this.Retas.PerformLayout();
-            this.Circunferencia.ResumeLayout(false);
-            this.Circunferencia.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -196,17 +207,18 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl;
-        private System.Windows.Forms.TabPage Retas;
-        private System.Windows.Forms.RadioButton rbPMR;
-        private System.Windows.Forms.RadioButton rbDDA;
-        private System.Windows.Forms.RadioButton rbEqReta;
-        private System.Windows.Forms.TabPage Circunferencia;
-        private System.Windows.Forms.RadioButton rbPMC;
-        private System.Windows.Forms.RadioButton rbTrigo;
-        private System.Windows.Forms.RadioButton rbEqCircu;
         private System.Windows.Forms.PictureBox picBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.CheckBox chekBoxReta;
+        private System.Windows.Forms.TabPage Retas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbPMC;
+        private System.Windows.Forms.RadioButton rbPMR;
+        private System.Windows.Forms.RadioButton rbTrigo;
+        private System.Windows.Forms.RadioButton rbDDA;
+        private System.Windows.Forms.RadioButton rbEqCircu;
+        private System.Windows.Forms.RadioButton rbEqReta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
 
--- Retas/TrabalhoCG/TrabalhoCG/Form1.cs
+++ Retas/TrabalhoCG/TrabalhoCG/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -34,6 +35,7 @@ namespace TrabalhoCG
 
         private void picBox1_MouseClick(object sender, MouseEventArgs e)
         {
+
             if (x1 != -1)
             {
                 x2 = e.X;
@@ -58,7 +60,14 @@ namespace TrabalhoCG
                     */
 
                 }
-                desenharReta(x1,y1,x2,y2,Color.Black);
+                if( rbEqReta.Checked )
+                    desenharReta(x1,y1,x2,y2,Color.Black);
+                else
+                    if(rbDDA.Checked )
+                        DDA(x1,y1,x2,y2,Color.Red);
+                else
+                    if(rbPMR.Checked )
+                        bresenham1(x1,y1,x2,y2, Color.Blue);
                 x1=x2= y2 = y1= -1;
             }
             else
@@ -92,6 +101,124 @@ namespace TrabalhoCG
         } 
         */
 
+        public void DDA(int x1, int y1, int x2, int y2, Color cor)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+            float xInc = dx / (float)steps;
+            float yInc = dy / (float)steps;
+
+            float x = x1;
+            float y = y1;
+
+            BitmapData bitmapDatabmp = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format24bppRgb);
+
+            unsafe
+            {
+                byte* pscan0 = (byte*)bitmapDatabmp.Scan0.ToPointer();
+                byte* pos;
+
+                for (int i = 0; i <= steps; i++)
+                {
+                    int xPos = (int)Math.Round(x);
+                    int yPos = (int)Math.Round(y);
+
+                    if (xPos >= 0 && xPos < bmp.Width &&
+                        yPos >= 0 && yPos < bmp.Height)
+                    {
+                        pos = pscan0 + (xPos * 3) + (yPos * bitmapDatabmp.Stride);
+                        pos[0] = cor.B;
+                        pos[1] = cor.G;
+                        pos[2] = cor.R;
+                    }
+
+                    x += xInc;
+                    y += yInc;
+                }
+            }
+
+            bmp.UnlockBits(bitmapDatabmp);
+            picBox1.Refresh();
+        }
+
+        public void bresenham1(int x1, int y1, int x2, int y2, Color cor)
+        {
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+
+            int sx = (x1 < x2) ? 1 : -1;
+            int sy = (y1 < y2) ? 1 : -1;
+
+            int err = dx - dy;
+
+            BitmapData bitmapDatabmp = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format24bppRgb);
+
+            unsafe
+            {
+                byte* pscan0 = (byte*)bitmapDatabmp.Scan0.ToPointer();
+                byte* pos;
+
+                while (true)
+                {
+                    if (x1 >= 0 && x1 < bmp.Width && y1 >= 0 && y1 < bmp.Height)
+                    {
+                        pos = pscan0 + (x1 * 3) + (y1 * bitmapDatabmp.Stride);
+                        pos[0] = cor.B;
+                        pos[1] = cor.G;
+                        pos[2] = cor.R;
+                    }
+
+                    if (x1 == x2 && y1 == y2)
+                        break;
+
+                    int e2 = 2 * err;
+
+                    if (e2 > -dy)
+                    {
+                        err -= dy;
+                        x1 += sx;
+                    }
+
+                    if (e2 < dx)
+                    {
+                        err += dx;
+                        y1 += sy;
+                    }
+                }
+            }
+
+            bmp.UnlockBits(bitmapDatabmp);
+            picBox1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            rbDDA.Checked = false;
+            rbEqCircu.Checked = false;
+            rbEqReta.Checked = false;
+            rbTrigo.Checked = false;
+            rbPMC.Checked = false;
+            rbPMR.Checked = false;
+        }
 
         void writeAllPixel(Color cor)
         {
@@ -118,7 +245,7 @@ namespace TrabalhoCG
 
 
 
-        public void desenharReta(int x1, int y1, int x2, int y2, Color cor)
+        /*public void desenharReta(int x1, int y1, int x2, int y2, Color cor)
         {
             double dy = y2 - y1;
             double dx = x2 - x1;
@@ -157,18 +284,83 @@ namespace TrabalhoCG
                         pos[2] = cor.R;
                     }
                 }
-
-
             }
+            bmp.UnlockBits(bitmapDatabmp);
+            picBox1.Refresh();
+        }*/
+
+        public void desenharReta(int x1, int y1, int x2, int y2, Color cor)
+        {
+            double dy = y2 - y1;
+            double dx = x2 - x1;
+
+            BitmapData bitmapDatabmp = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format24bppRgb);
+
+            unsafe
+            {
+                byte* pscan0 = (byte*)bitmapDatabmp.Scan0.ToPointer();
+                byte* pos;
+
+                // 🔹 Caso reta vertical
+                if (dx == 0)
+                {
+                    int yStart = Math.Min(y1, y2);
+                    int yEnd = Math.Max(y1, y2);
+
+                    for (int y = yStart; y <= yEnd; y++)
+                    {
+                        pos = pscan0 + (x1 * 3) + (y * bitmapDatabmp.Stride);
+                        pos[0] = cor.B;
+                        pos[1] = cor.G;
+                        pos[2] = cor.R;
+                    }
+                }
+                else
+                {
+                    double m = dy / dx;
+
+                    if (Math.Abs(dy) > Math.Abs(dx))
+                    {
+                        int yStart = Math.Min(y1, y2);
+                        int yEnd = Math.Max(y1, y2);
+
+                        for (int y = yStart; y <= yEnd; y++)
+                        {
+                            double x = x1 + (y - y1) / m;
+                            int xPos = (int)Math.Round(x);
+
+                            pos = pscan0 + (xPos * 3) + (y * bitmapDatabmp.Stride);
+                            pos[0] = cor.B;
+                            pos[1] = cor.G;
+                            pos[2] = cor.R;
+                        }
+                    }
+                    else
+                    {
+                        int xStart = Math.Min(x1, x2);
+                        int xEnd = Math.Max(x1, x2);
+
+                        for (int x = xStart; x <= xEnd; x++)
+                        {
+                            double y = y1 + m * (x - x1);
+                            int yPos = (int)Math.Round(y);
+
+                            pos = pscan0 + (x * 3) + (yPos * bitmapDatabmp.Stride);
+                            pos[0] = cor.B;
+                            pos[1] = cor.G;
+                            pos[2] = cor.R;
+                        }
+                    }
+                }
+            }
+
             bmp.UnlockBits(bitmapDatabmp);
             picBox1.Refresh();
         }
 
-        private void chekBoxReta_CheckedChanged(object sender, EventArgs e)
-        {
-
-            //Color cor = Color.Red;
-            //writeAllPixel(cor);
-        }
+        
     }
 }
